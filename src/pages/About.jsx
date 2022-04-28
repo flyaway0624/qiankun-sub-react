944c9e086376f5d3a9734f836c114dbf8175e586
JIANG NING <njiang93@best-inc.com>
2022-04-28 10:47:14 +0800

    firsdt commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About(props) {
+  console.log(props)
+  return (
+    <h2 className="app-nav-item" style={{ borderColor: 'green' }}>
+      About
+    </h2>
+  );
+}
+export default About;
